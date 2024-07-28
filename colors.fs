b9ce7fcba9516ce3991cd00c17824a7d99ddc5f4
Ross Charles Campbell <rossbridger.cc@protonmail.com>
2024-07-28 23:09:40 +0800

    Implement point light.

--- colors.fs
+++ colors.fs
@@ -1,4 +1,6 @@
 #version 330 core
+out vec4 FragColor;
+
 struct Material {
     sampler2D diffuse;
 	sampler2D specular;
@@ -6,40 +8,49 @@ struct Material {
 };
 
 struct Light {
-    vec3 direction;
-	vec3 ambient;
+    vec3 position;
+
+    vec3 ambient;
 	vec3 diffuse;
 	vec3 specular;
+
+    float constant;
+	float linear;
+	float quadratic;
 };
-
-out vec4 FragColor;
-
-uniform vec3 objectColor;
-uniform vec3 viewPos;
-uniform Material material;
-uniform Light light;
 
 in vec3 FragPos;
 in vec3 Normal;
 in vec2 TexCoords;
 
+uniform vec3 viewPos;
+uniform Material material;
+uniform Light light;
+
 void main()
 {
-    vec3 lightDir = normalize(-light.direction);
-
     // ambient
-	vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
+	vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb;
 
     // diffuse
 	vec3 norm = normalize(Normal);
+	vec3 lightDir = normalize(light.position - FragPos);
 	float diff = max(dot(norm, lightDir), 0.0);
-	vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));
+	vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb;
 
     // specular
 	vec3 viewDir = normalize(viewPos - FragPos);
 	vec3 reflectDir = reflect(-lightDir, norm);
 	float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-	vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
+	vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb;
+
+    // attenuation
+	float distance    = length(light.position - FragPos);
+	float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
+
+    ambient  *= attenuation;
+	diffuse   *= attenuation;
+	specular *= attenuation;
 
     vec3 result = ambient + diffuse + specular;
 	FragColor = vec4(result, 1.0);
