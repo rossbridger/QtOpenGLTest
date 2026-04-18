f63536fde533147ab3a0b5a07fcdd066227c79a3
Ross Charles C. <10781734+rossbridger@users.noreply.github.com>
2026-04-18 10:17:01 +0700

    Add a side pane to the main window.
    Signed-off-by: Ross Charles C. <10781734+rossbridger@users.noreply.github.com>

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,25 @@
+#include <QHBoxLayout>
+#include <qtextedit.h>
 #include "mainwindow.h"
 #include "openglwidget.h"
 
 MainWindow::MainWindow(QWidget *parent)
-	: QMainWindow(parent)
+	: QWidget(parent)
 {
-	OpenGLWidget *widget = new OpenGLWidget(this);
-	setCentralWidget(widget);
+	QWidget* side_pane = new QWidget();
+	side_pane->setFixedWidth(200);
+	side_pane->setLayout(new QVBoxLayout(side_pane));
+	side_pane->layout()->addWidget(new QTextEdit());
+	// TODO: design side pane to show configurable params.
+
+	OpenGLWidget *widget = new OpenGLWidget();
+	QHBoxLayout* layout = new QHBoxLayout(this);
+	layout->setContentsMargins(0, 0, 0, 0);
+	layout->addWidget(side_pane);
+	layout->addWidget(widget);
 	widget->setMinimumWidth(800);
 	widget->setMinimumHeight(600);
+	setLayout(layout);
 }
 
 MainWindow::~MainWindow() {}
-
