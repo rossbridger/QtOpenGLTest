4a9be74a24c55e6c88c578b11a2fbdda970b767a
Ross Charles C. <10781734+rossbridger@users.noreply.github.com>
2026-04-20 20:37:54 +0700

    Add lighting uniforms to the model shader, prep for lighting.
    Signed-off-by: Ross Charles C. <10781734+rossbridger@users.noreply.github.com>

--- model.fs
+++ model.fs
@@ -4,9 +4,33 @@ layout(location = 0) out vec4 FragColor;
 in vec2 TexCoords;
 
 uniform sampler2D texture_diffuse1;
-uniform sampler2D texture_specular;
-uniform sampler2D texture_normal;
-uniform sampler2D texture_height;
+uniform sampler2D texture_specular1;
+uniform sampler2D texture_normal1;
+uniform sampler2D texture_height1;
+
+struct DirLight {
+    vec3 direction;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+uniform DirLight dirLight;
+
+struct PointLight {
+    vec3 position;
+
+    float constant;
+    float linear;
+    float quadratic;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+#define NR_POINT_LIGHTS 4
+uniform PointLight pointLights[NR_POINT_LIGHTS];
 
 void main()
 {
